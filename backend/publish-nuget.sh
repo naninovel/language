785ac8c36129587f55aa15d5e98f10575148545f
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-03-07 15:15:03 +0300

    fix publish script

--- backend/publish-nuget.sh
+++ backend/publish-nuget.sh
@@ -3,5 +3,5 @@ rm *.nupkg
 cd ..
 dotnet pack --configuration Release --output .nuget
 cd .nuget
-dotnet nuget push "*.nupkg" --source "github"
+dotnet nuget push "*.nupkg" --source "naninovel"
 read -r -p "Press Enter key to exit..."
