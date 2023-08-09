6f9cecb164dcf22f5023223e10010d6c151d643c
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-09 17:49:21 +0300

    update specs

--- backend/Naninovel.Language.Test/Diagnostic/MultipleDiagnosersTest.cs
+++ backend/Naninovel.Language.Test/Diagnostic/MultipleDiagnosersTest.cs
@@ -12,7 +12,7 @@ public class MultipleDiagnosersTest : DiagnoserTest
         Docs.SetupScript("other.nani", "#");
         Meta.SetupCommandWithEndpoint("goto");
         Assert.Equal(2, Diagnose("@goto other.label p:").Count);
-        Assert.Equal(1, Diagnose("@goto other.label p:v").Count);
+        Assert.Single(Diagnose("@goto other.label p:v"));
         Handler.HandleDocumentRemoved(DefaultUri);
         Handler.HandleDocumentRemoved("other.nani");
         Assert.Empty(GetDiagnostics());
